aa3c3db7ccbdbda4c3fc28f93be4f97e1f6b879e
mariamsanou <79387601+mariamsanou@users.noreply.github.com>
2024-04-22 19:39:01 -0400

    Create Menubar.js

---
+++ src/Menubar.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+//import { useNavigate } from 'react-router-dom';
+
+function Menubar( ) {
+    const [showMenu, setShowMenu] = useState(false);
+    //const navigate = useNavigate();
+    const loggedInUser = localStorage.getItem('loggedInUser');
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    };
+
+    return (
+        <div>
+            <button onClick={toggleMenu}>Menu Bar</button>
+            {showMenu && (
+                <div>
+                    {loggedInUser ? (
+                        <>
+                            <div>
+                                <Link to="/dashboard">Dashboard</Link>
+                            </div>
+                            <div>
+                                <Link to="/viewEvent">View Event</Link>
+                            </div>
+                            <div>
+                                <Link to="/createWishlist">Create Wishlist</Link>
+                            </div>
+                            <div>
+                                <Link to="/viewReceiver">View Receiver</Link>
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            <div>
+                                <Link to="/login">Login</Link>
+                            </div>
+                            <div>
+                                <Link to="/signup">Signup</Link>
+                            </div>
+                            <div>
+                                <Link to="/moreInfo">More Info</Link>
+                            </div>
+                        </>
+                    )}
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default Menubar;
